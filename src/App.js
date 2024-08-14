d2c67cf957d059a07f982044d779268f475c77b5
PyroAnnatar <pyroannatar@gmail.com>
2024-08-14 19:05:41 +0300

    More SSR Bull

--- src/App.js
+++ src/App.js
@@ -3,26 +3,34 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 // Bileşenin bağlantısı kaldırıldığında/unmounted uygulamamızın state'i güncellemesini durdurmak için useEffect'teki cleanup fonksiyonunu kullanın
 export default function App() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <div className="flex justify-center flex-col items-center py-8">
       <h1 className="text-2xl font-bold pb-4">🐭</h1>
-      <BrowserRouter>
-        <nav className="flex justify-center max-w-sm p-4 pb-8 space-x-4">
-          <Link className="underline" to="/">
-            Home
-          </Link>
-          <Link className="underline" to="/about">
-            About
-          </Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<MousePosition />} />
-          <Route
-            path="/about"
-            element={<h1>Mouse&apos;unuzu takip edin!</h1>}
-          />
-        </Routes>
-      </BrowserRouter>
+      {isClient && (
+        <BrowserRouter>
+          <nav className="flex justify-center max-w-sm p-4 pb-8 space-x-4">
+            <Link className="underline" to="/">
+              Home
+            </Link>
+            <Link className="underline" to="/about">
+              About
+            </Link>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MousePosition />} />
+            <Route
+              path="/about"
+              element={<h1>Mouse&apos;unuzu takip edin!</h1>}
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
@@ -35,9 +43,9 @@ function MousePosition() {
       setPosition({ x: e.clientX, y: e.clientY });
       console.log("Updating state");
     }
-    if (typeof window !== "undefined")
+    if (typeof window !== "undefined") {
       window.addEventListener("pointermove", handleMove);
-
+    }
     return () => {
       if (typeof window !== "undefined") {
         window.removeEventListener("pointermove", handleMove);
